9582a8f982845bc0407621a17dea0452c17a123c
Michael Carrell <40646886+michaeldcarrell@users.noreply.github.com>
2025-07-07 11:20:31 -0600

    Update middleware.py

--- simpli_budget/middleware.py
+++ simpli_budget/middleware.py
@@ -6,11 +6,12 @@ class SetUserAttributeDefaults:
         self.get_response = get_response
 
     def __call__(self, request):
-        if not UserAttributes.objects.filter(
-            user=request.user
-        ).exists():
-            UserAttributes.objects.create(
-                user=request.user,
-                show_hidden=False
-            )
+        if request.user.is_authenticated:
+            if not UserAttributes.objects.filter(
+                user=request.user
+            ).exists():
+                UserAttributes.objects.create(
+                    user=request.user,
+                    show_hidden=False
+                )
         return self.get_response(request)
